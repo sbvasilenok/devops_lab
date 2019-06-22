d082fe9bd272930bbc43ffb0063c0e75f89d0426
Sergey Vasilenok <sergey_vasilenok@epam.com>
2019-06-22 18:00:05 +0300

    app version 1.3 build
    updated fs exceptions
    
    update README.md

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-DevOps Lab 2019 (May-August)
+### Install
+To install package switch to test environment (optional), then run in console:
+$ pip install *.whl
+
+### Usage 
+To start application simply run in console:
+$ svsyslogger
+
+### Config 
+You can set output parameters in config.conf which is in `...site-packages/svsyslogger/` directory. You could see the full path to config when app is starting.
+For output in json format set option `output=json`
+For output in plain text set `output=plain` or remove/comment "output" option.
+For change logging interval set "interval" parameter in seconds. If not present, default is 300s.
+For change path where logs are saving set "logpath" to appropriate directory. If not present, logfile would be created in `...site-packages/svsyslogger/` directory. You could see the full path to logfile when app is starting.
---
+++ core.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+from datetime import datetime
+import time
+import os
+import configparser
+import psutil
+import json
+
+
+class JsonLogger:
+
+    snapnumber = 0
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def logtofile(self):
+
+        filename = self.filename
+        JsonLogger.snapnumber += 1
+
+        data = {
+            'SNAPSHOT': JsonLogger.snapnumber,
+            'TIME': datetime.now().strftime('%d-%m-%Y %H:%M:%S'),
+            'CPU': str(psutil.cpu_percent(interval=1)),
+            'MEMORY': str(psutil.virtual_memory()),
+            'SWAP': str(psutil.swap_memory()),
+            'IO': str(psutil.disk_io_counters(perdisk=False)),
+            'NETWORK': str(psutil.net_io_counters(pernic=True))
+        }
+
+        with open(filename, 'a+', encoding='utf-8') as logfile:
+            try:
+                json.dump(data, logfile, ensure_ascii=False, indent=1)
+            except NameError:
+                print('write error')
+
+
+class PlainLogger:
+
+    snapnumber = 0
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def logtofile(self):
+
+        filename = self.filename
+        PlainLogger.snapnumber += 1
+
+        data = [
+            'SNAPSHOT ',
+            str(PlainLogger.snapnumber),
+            datetime.now().strftime(': %d-%m-%Y %H:%M:%S :'),
+            ' CPU: ', str(psutil.cpu_percent(interval=1)),
+            ' MEMORY: ', str(psutil.virtual_memory()),
+            ' SWAP: ', str(psutil.swap_memory()),
+            ' IO: ', str(psutil.disk_io_counters(perdisk=False)),
+            ' NETWORK: ', str(psutil.net_io_counters(pernic=True)), '\n'
+        ]
+
+        with open(filename, 'a+', encoding='utf-8') as logfile:
+            try:
+                logfile.writelines(data)
+            except NameError:
+                print('write error')
+
+
+def main():
+
+    cfgfilename = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'config.conf')
+
+    if not os.path.isfile(cfgfilename):
+        interval = 300
+        logtype = 'plain'
+        print('config.conf not exist, defaults loaded')
+    else:
+        try:
+            config = configparser.ConfigParser()
+            config.read(cfgfilename)
+        except NameError:
+            interval = 300
+            logtype = 'plain'
+            print('config.conf read error, defaults loaded')
+        finally:
+            try:
+                interval = int(config['common']['interval'])
+            except KeyError:
+                interval = 300
+            try:
+                logtype = config['common']['output']
+            except KeyError:
+                logtype = 'plain'
+            try:
+                logpath = config['common']['logpath']
+            except KeyError:
+                logpath = os.path.abspath(os.path.dirname(__file__))
+            finally:
+                if not os.path.exists(logpath):
+                    try:
+                        os.makedirs(logpath)
+                    except OSError:
+                        logpath = os.path.abspath(os.path.dirname(__file__))
+                print('path to config:', cfgfilename)
+
+    if logtype == 'json':
+        logfilename = os.path.join(logpath, 'log.json')
+        logger = JsonLogger(logfilename)
+    else:
+        logfilename = os.path.join(logpath, 'log.txt')
+        logger = PlainLogger(logfilename)
+
+    print('path to logfile:', logfilename)
+
+    while True:
+        logger.logtofile()
+        time.sleep(interval)
+
+
+if __name__ == '__main__':
+    main()
